3215a86aa837d758552e93e5fc0ada8d6c474444
AndreasWituschek <andreas.wituschek@gmx.de>
2019-02-23 22:10:44 +0100

    implemented 4 channels and filtering for modfreq and I0 in masterfilewriter.py
    Importing of MFLI data from mfli.py has still to be written (Daniel)

--- fermi_analysis/functions.py
+++ fermi_analysis/functions.py
@@ -6,6 +6,8 @@ Created on Wed Feb 13 13:22:02 2019
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+import pandas as pd
 
 #physical constants:
 c=299.792458 #speed of light in nm/fs
@@ -113,7 +115,7 @@ def plotFdCurveEV(stop,start,cdft,cdft_d,l_ref,l_He,h):
     plt.legend()
     plt.title("Absorptive and dispersive spectra")
 
-def ReadMfliData(bunches): #bunches= number of shots in one file
+def ReadMfliData(mfli_file_path,bunches): #bunches= number of shots in one file
     "reads data from files created with mfli.py containing the raw demodulated data. ONLY DUMMY TO CREATE SOME DATA RIGHT NOW"
     X = [np.random.rand(bunches) for i in np.arange(0,4)] #demodulated X data for each bunch
     Y = [np.random.rand(bunches) for i in np.arange(0,4)] #demodulated Y data for each bunch
@@ -129,18 +131,50 @@ def ReadMfliData(bunches): #bunches= number of shots in one file
                  'Y': Y,} #write all into one dictionary
     return mfli_data
     
-def AveragingMfliData(mfli_data,I0,apply_filter,I0_threshold,modfreq_set,modfreq_threshold):
-    b = [a and b for a, b in zip([i>I0_threshold for i in I0], [-modfreq_threshold < i-modfreq_set < modfreq_threshold for i in mfli_data['modfreq']]) ] #boolean for filtering of I0 and modfreq
+def AveragingMfliData(mfli_data,I0,apply_filter,I0_threshold,modfreq_set,modfreq_threshold,ignore):
+    "Applys filters (IO and modfreq) on mfli data and averages filtered values"
     if apply_filter:
-        mX= [np.mean(mfli_data['X'][i][np.where(b)]) for i in range(len(mfli_data['X']))]
-        mY= [np.mean(mfli_data['Y'][i][np.where(b)]) for i in range(len(mfli_data['Y']))]
-        sX= [np.std(mfli_data['X'][i][np.where(b)]) for i in range(len(mfli_data['X']))]
-        sY= [np.std(mfli_data['Y'][i][np.where(b)]) for i in range(len(mfli_data['Y']))]
-    else: 
-        mX= [np.mean(mfli_data['X'][i]) for i in range(len(mfli_data['X']))]
-        mY= [np.mean(mfli_data['Y'][i]) for i in range(len(mfli_data['Y']))]
-        sX= [np.std(mfli_data['X'][i]) for i in range(len(mfli_data['X']))]
-        sY= [np.std(mfli_data['Y'][i]) for i in range(len(mfli_data['Y']))]
-    return mX,mY,sX,sY
+        I0_filter=np.asarray([i>I0_threshold for i in I0])*1 #I0 filter
+        I0_filter=np.concatenate([I0_filter,np.ones(ignore)])
+        a=np.zeros((ignore,len(I0_filter)))
+        for i in range(ignore):
+            a[i]=np.roll(I0_filter,i)
+        I0_filter=(np.sum(a,0)>(ignore-1))[ignore:len(I0)+ignore] #I0 filter extended by number of ignored shots
+        
+        modfreq_filter= np.asarray([-modfreq_threshold < i-modfreq_set < modfreq_threshold for i in mfli_data['modfreq']]) #filter for modfreq
+        modfreq_filter=np.concatenate([modfreq_filter,np.ones(ignore)])
+        a=np.zeros((ignore,len(modfreq_filter)))
+        for i in range(ignore):
+            a[i]=np.roll(modfreq_filter,i)
+        modfreq_filter=(np.sum(a,0)>(ignore-1))[ignore:len(I0)+ignore] #modfreq filter extended by number of ignored shots
 
-         
+        b = np.logical_and(I0_filter,modfreq_filter) #combining both filters above
+                
+        if sum(b)>0:
+            print("# total bunches = %d" % len(I0))
+            print("# filtered Bunches = %d" % sum(b))
+            mX= [np.mean(mfli_data['X'][i][np.where(b)]) for i in range(len(mfli_data['X']))]
+            mY= [np.mean(mfli_data['Y'][i][np.where(b)]) for i in range(len(mfli_data['Y']))]
+            sX= [np.std(mfli_data['X'][i][np.where(b)]) for i in range(len(mfli_data['X']))]
+            sY= [np.std(mfli_data['Y'][i][np.where(b)]) for i in range(len(mfli_data['Y']))]
+        else:
+            print('Filters to tight, no data to average over. Script stopped!')
+            sys.exit(1)
+    else: 
+        mX= np.mean((mfli_data['X']),1)
+        mY= np.mean((mfli_data['Y']),1)
+        sX= np.std((mfli_data['X']),1)
+        sY= np.std((mfli_data['Y']),1)
+        print("# averaged bunches = %d" % len(I0))
+    return mX,mY,sX,sY
+    
+def MasterFileWriter(mfli_data,delay,mX,mY,sX,sY):
+    d = {'run': [mfli_data['run']], 'delay': [delay],'mX0': mX[0], 'mY0': mY[0],'mX1': mX[1],'mY1': mY[1],'mX2': mX[2],'mY2': mY[2],'mX3': mX[3],'mY3': mY[3],'sX0': sX[0],'sY0': sY[0],'sX1': sX[1],'sY1': sY[1],'sX2': sX[2],'sY2': sY[2],'sX3': sX[3],'sY3': sY[3],'bunchnumber': [mfli_data['bunchnumber']],'timestamp' : [mfli_data['timestamp']]}
+    df = pd.DataFrame.from_dict(d)    
+    return df
+
+
+def DelayMove(delay_pos):
+    if all(x == delay_pos[0] for x in delay_pos):
+        delay_pos=delay_pos[0]
+    else: print "Delaystage moved during run!"
--- masterfilewriter.py
+++ masterfilewriter.py
@@ -11,66 +11,47 @@ Reads hd5 files from LDM data acquisition system and hd5 files from mfli.py prog
 import h5py
 import numpy as np
 import fermi_analysis.functions as fk
-import pandas as pd
 
+'''Experimental parameters '''
+ri=1.6116 #refractive index of wedges at seed wavelenght
+delay_zero_pos= -19.8 #position of delay stage in mm where we have temporal overlap between seed pulses
 
 ''' FILE I/O '''
 #importing LDM data:
-file_path='//nanoserver/user/Projekte/BMBF/FERMI Beispieldaten/Nh3_044_72274751.h5'
-ldm_data = h5py.File(file_path, 'r')
+ldm_file_path='//nanoserver/user/Projekte/BMBF/FERMI Beispieldaten/Nh3_044_72274751.h5'
+mfli_file_path = "//nanoserver/user/Projekte/BMBF/FERMI Beispieldaten/test.h5"
+
+ldm_data = h5py.File(ldm_file_path, 'r')
+
 I0 = np.array(ldm_data['photon_diagnostics']['FEL01']['I0_monitor']['iom_sh_a']) # I0 in uJ
-delay = np.array(ldm_data['user_laser']['delay_line']['position']) #position of delay line
+delay_pos = np.array(ldm_data['user_laser']['delay_line']['position']) #position of delay stage
 bunches= len(I0) # number of bunches per file
 
 #importing MFLI data:
-mfli_data = fk.ReadMfliData(bunches)
+mfli_data = fk.ReadMfliData(mfli_file_path,bunches)
 
 ''' Data filtering '''
-apply_filter = False #choose wether to apply following filters:
-I0_threshold= np.mean(I0)-2*np.std(I0) #lower threshold for I0 power filtering
+apply_filter = True #choose wether to apply following filters:
+ignore=1 #number of bunches to ignore after filter incident (depends on MFLI time constant)
+I0_threshold= np.mean(I0)-3*np.std(I0) #lower threshold for I0 power filtering
 modfreq_set = 1000. # modulation frequency in Hz set by the user at the AD9959 board
-modfreq_threshold= 0.9 #disregards data when not within +- modfreq_threshold from modfreq_set because then MFLI was not properly locked
+modfreq_threshold= .9 #[Hz] Disregards data when not within modfreq_set +- modfreq_threshold because then MFLI was not properly locked
 
+fk.DelayMove(delay_pos) #check if delaystage moved during run.
 
 ''' processing data '''
-if all(x == delay[0] for x in delay):
-    delay=delay[0]
-else: print "Delaystage moved during run!"
-
 #average values and standard deviation of filtered demodulated values
-mX,mY,sX,sY=fk.AveragingMfliData(mfli_data,I0,apply_filter,I0_threshold,modfreq_set,modfreq_threshold)
+mX,mY,sX,sY=fk.AveragingMfliData(mfli_data,I0,apply_filter,I0_threshold,modfreq_set,modfreq_threshold,ignore)
+delay= (delay_pos - delay_zero_pos)*(ri-1)*np.sin(7./180*np.pi)/0.000299792458 #convert position [mm] of delay line to fs
+#delay=4
 
 ''' Writing to masterfile'''
-#write all data in dictionary:
-d = {'run': [mfli_data['run']], 
-     'delay': [delay],
-     'mX0': mX[0], 
-     'mY0': mY[0],
-     'mX1': mX[1],
-     'mY1': mY[1],
-     'mX2': mX[2],
-     'mY2': mY[2],
-     'mX3': mX[3], 
-     'mY3': mY[3],     
-     'sX0': sX[0], 
-     'sY0': sY[0],
-     'sX1': sX[1],
-     'sY1': sY[1],
-     'sX2': sX[2],
-     'sY2': sY[2],
-     'sX3': sX[3], 
-     'sY3': sY[3],
-     'bunchnumber': [mfli_data['bunchnumber']],
-     'timestamp' : [mfli_data['timestamp']]}
+#write all data to one dataframe:
+df=fk.MasterFileWriter(mfli_data,delay,mX,mY,sX,sY)
 
-
-df = pd.DataFrame.from_dict(d) #converting dict to data frame
-
-##Create a CSV file:
+##Create a CSV file. Do this when running this program for the first time. Otherwise this will overwrite existing masterData.csv file. 
 #df.to_csv("masterData.csv")
 
 #appending data to masterData.csv File
 with open('masterData.csv', 'a') as f:
     df.to_csv(f, header=False)
-
-
